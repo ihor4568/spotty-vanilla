ae04ddb023f08c5cb10f8b20b730471327f2899e
Ivan Yevsiutin <gamesminer123@gmail.com>
2018-10-28 01:22:42 +0300

    KHWE-13: Implement SharingSong component to Main&some fixes
    Added component SharingSong and connected it to the Main. Changed width
    of content on 100%. Some fixes in SharingSong.js & SharingSong.html in
    connection with the comments to previous teammate in order to the task.
    Move image to another dist (./assets/...)

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import { MDCDrawer } from "@material/drawer";
 import { PlayerComponent } from "../Player/Player";
 import { HeaderComponent } from "../Header/Header";
 import { SearchComponent } from "../Search/Search";
+import { SharingSongComponent } from "../SharingSong/SharingSong";
 import mainTemplate from "./Main.html";
 
 export class MainComponent {
@@ -16,6 +17,7 @@ export class MainComponent {
     this.headerPoint = this.mountPoint.querySelector(".main__header");
     this.playerPoint = this.mountPoint.querySelector(".main__player");
     this.searchPoint = this.mountPoint.querySelector(".main__search");
+    this.shareViewPoint = this.mountPoint.querySelector(".main__share-view");
   }
 
   initMaterial() {
@@ -47,6 +49,9 @@ export class MainComponent {
 
     this.search = new SearchComponent(this.searchPoint);
     this.search.mount();
+
+    this.sharingSong = new SharingSongComponent(this.shareViewPoint);
+    this.sharingSong.mount();
   }
 
   render() {
