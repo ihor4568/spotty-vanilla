df865f109d8b79a51738914f083c3ee8634d92f0
Ihor Tsarenko <tsarenko.igor1@gmail.com>
2018-10-30 09:14:42 +0200

    KHWE-7 delete useless code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import "./index.scss";
 import { MainComponent } from "./components/Main/Main";
-import { MySongsViewTableComponent } from "./components/MySongsView/MySongsViewTable/MySongsViewTable";
 
 const root = document.getElementById("root");
 
 const main = new MainComponent(root);
 
 main.mount();
-
-
