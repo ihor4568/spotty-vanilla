e141552c7c5dc6dad9c6619a2502da00420d3015
Yevhenii Ivanisenko <yevhenii.ivanisenko@gmail.com>
2018-11-01 00:13:36 +0200

    Add createAlbum function

--- src/index.js
+++ src/index.js
@@ -6,7 +6,3 @@ const root = document.getElementById("root");
 const main = new MainComponent(root);
 
 main.mount();
-
-// Test firebase methods. Don't forget to uncomment firebaseCreationScripts. I commented it out to pass eslint tests)
-// firebaseCreationScripts.createAlbum("FrostWire");
-// firebaseCreationScripts.createAlbum("netBloc Vol. 33");
