6f9d1c9c93abc9617cb2ccf8195baf217693964e
WOLFRIEND <wolfriend777@gmail.com>
2018-11-01 14:54:25 +0300

    Replace ArtistsData.js to AtistsComponent

--- src/components/Artists/Artists.js
+++ src/components/Artists/Artists.js
@@ -1,10 +1,64 @@
 import { MDCRipple } from "@material/ripple";
 import artistsTemplate from "./Artists.html";
 
+const ARTISTS_DATA = [
+  {
+    name: "Nicki Minaj",
+    cover: "https://bit.ly/2Q4xgEZ",
+    link: "https://www.instagram.com/nickiminaj/"
+  },
+  {
+    name: "Eminem",
+    cover: "https://bit.ly/2Q4xqfz",
+    link: "https://www.instagram.com/eminem/"
+  },
+  {
+    name: "50 Cent",
+    cover: "https://bit.ly/2yGMtWs",
+    link: "https://www.instagram.com/50cent/"
+  },
+  {
+    name: "Dr. Dre",
+    cover: "https://bit.ly/2DeyScx",
+    link: "https://www.instagram.com/drdre/"
+  },
+  {
+    name: "Ice Cube",
+    cover: "https://bit.ly/2CRLPZ4",
+    link: "https://www.instagram.com/icecube/"
+  },
+  {
+    name: "Tech N9ne",
+    cover: "https://bit.ly/2OmMUKA",
+    link: "https://www.instagram.com/therealtechn9ne/"
+  },
+  {
+    name: "Redman",
+    cover: "https://bit.ly/2CQCsbQ",
+    link: "https://www.instagram.com/redmangilla/"
+  },
+  {
+    name: "Cassidy",
+    cover: "https://bit.ly/2PxjNJ3",
+    link: "https://www.instagram.com/cassidy_larsiny/"
+  },
+  {
+    name: "Busta Rhymes",
+    cover: "https://bit.ly/2QeXJQE",
+    link: "https://www.instagram.com/bustarhymes/"
+  },
+  {
+    name: "Dizaster",
+    cover: "https://bit.ly/2RpRDgk",
+    link: "https://www.instagram.com/mrdizaster/"
+  }
+];
+
 export class ArtistsComponent {
   constructor(mountPoint, props = {}) {
     this.mountPoint = mountPoint;
     this.props = props;
+    this.artistsData = ARTISTS_DATA;
   }
 
   querySelectors() {
@@ -24,6 +78,6 @@ export class ArtistsComponent {
   }
 
   render() {
-    return artistsTemplate({ artistsData: this.props.artistsData });
+    return artistsTemplate({ artistsData: this.artistsData });
   }
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,64 +7,10 @@ import { AboutComponent } from "../About/About";
 import { ArtistsComponent } from "../Artists/Artists";
 import mainTemplate from "./Main.html";
 
-const ARTISTS_DATA = [
-  {
-    name: "Nicki Minaj",
-    cover: "https://bit.ly/2Q4xgEZ",
-    link: "https://www.instagram.com/nickiminaj/"
-  },
-  {
-    name: "Eminem",
-    cover: "https://bit.ly/2Q4xqfz",
-    link: "https://www.instagram.com/eminem/"
-  },
-  {
-    name: "50 Cent",
-    cover: "https://bit.ly/2yGMtWs",
-    link: "https://www.instagram.com/50cent/"
-  },
-  {
-    name: "Dr. Dre",
-    cover: "https://bit.ly/2DeyScx",
-    link: "https://www.instagram.com/drdre/"
-  },
-  {
-    name: "Ice Cube",
-    cover: "https://bit.ly/2CRLPZ4",
-    link: "https://www.instagram.com/icecube/"
-  },
-  {
-    name: "Tech N9ne",
-    cover: "https://bit.ly/2OmMUKA",
-    link: "https://www.instagram.com/therealtechn9ne/"
-  },
-  {
-    name: "Redman",
-    cover: "https://bit.ly/2CQCsbQ",
-    link: "https://www.instagram.com/redmangilla/"
-  },
-  {
-    name: "Cassidy",
-    cover: "https://bit.ly/2PxjNJ3",
-    link: "https://www.instagram.com/cassidy_larsiny/"
-  },
-  {
-    name: "Busta Rhymes",
-    cover: "https://bit.ly/2QeXJQE",
-    link: "https://www.instagram.com/bustarhymes/"
-  },
-  {
-    name: "Dizaster",
-    cover: "https://bit.ly/2RpRDgk",
-    link: "https://www.instagram.com/mrdizaster/"
-  }
-];
-
 export class MainComponent {
   constructor(mountPoint, props = {}) {
     this.mountPoint = mountPoint;
     this.props = props;
-    this.artistsData = ARTISTS_DATA;
   }
 
   querySelectors() {
@@ -132,9 +78,7 @@ export class MainComponent {
 
     this.about = new AboutComponent(this.mainPoint);
 
-    this.artist = new ArtistsComponent(this.mainPoint, {
-      artistsData: this.artistsData
-    });
+    this.artist = new ArtistsComponent(this.mainPoint);
   }
 
   render() {
