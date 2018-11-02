6adf254cd5e83b2609123e64d3fe42d6d3588f57
Ihor Tsarenko <tsarenko.igor1@gmail.com>
2018-11-02 10:50:01 +0200

    KHWE-7 change columns, relocate tableData to MySongTable

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,37 +8,10 @@ import { AlbumsComponent } from "../Albums/Albums";
 import { AboutComponent } from "../About/About";
 import mainTemplate from "./Main.html";
 
-const TABLE_DATA = [
-  {
-    cover: "https://images.unian.net/photos/2017_09/1505748424-6475.jpg",
-    name: "Alibaba",
-    duration: "9:15",
-    artist: "Folk",
-    album: "Super"
-  },
-  {
-    cover:
-      "https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/6b2ef062-6913-4a94-a265-7d75f4f91854/64.jpg",
-    name: "Alibaba",
-    duration: "9:15",
-    artist: "Folk",
-    album: "Super"
-  },
-  {
-    cover:
-      "https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930",
-    name: "Alibaba",
-    duration: "9:15",
-    artist: "Folk",
-    album: "Super"
-  }
-];
-
 export class MainComponent {
   constructor(mountPoint, props = {}) {
     this.mountPoint = mountPoint;
     this.props = props;
-    this.tableData = TABLE_DATA;
   }
 
   querySelectors() {
@@ -97,9 +70,7 @@ export class MainComponent {
     this.search.mount();
 
     this.about = new AboutComponent(this.mainPoint);
-    this.table = new MySongsTableComponent(this.mainPoint, {
-      tableData: this.tableData
-    });
+    this.table = new MySongsTableComponent(this.mainPoint);
 
     this.albums = new AlbumsComponent(this.mainPoint);
     this.albums.mount();
--- src/components/MySongsTable/MySongsTable.js
+++ src/components/MySongsTable/MySongsTable.js
@@ -2,10 +2,37 @@ import { MDCRipple } from "@material/ripple";
 import mySongsTableTemplate from "./MySongsTable.html";
 import { DotsMenuComponent } from "../DotsMenu/DotsMenu";
 
+const TABLE_DATA = [
+  {
+    cover: "https://images.unian.net/photos/2017_09/1505748424-6475.jpg",
+    name: "Alibaba",
+    duration: "9:15",
+    artist: "Jazz",
+    album: "Super"
+  },
+  {
+    cover:
+      "https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/6b2ef062-6913-4a94-a265-7d75f4f91854/64.jpg",
+    name: "Timon and Pumba",
+    duration: "9:15",
+    artist: "Folk",
+    album: "Nice"
+  },
+  {
+    cover:
+      "https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930",
+    name: "Alibaba",
+    duration: "9:15",
+    artist: "Sympho",
+    album: "The very best"
+  }
+];
+
 export class MySongsTableComponent {
   constructor(mountPoint, props = {}) {
     this.mountPoint = mountPoint;
     this.props = props;
+    this.tableData = TABLE_DATA;
   }
 
   querySelectors() {
@@ -44,6 +71,6 @@ export class MySongsTableComponent {
   }
 
   render() {
-    return mySongsTableTemplate({ tableData: this.props.tableData });
+    return mySongsTableTemplate({ tableData: this.tableData });
   }
 }
