a2b27a28f30954e7a1309ec36e037e23ed03a7b6
Yevhenii Ivanisenko <yevhenii.ivanisenko@gmail.com>
2018-11-02 14:59:32 +0200

    Add eslint rule to prevent error on unresolved imports

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import "firebase/database";
 import "firebase/storage";
 
-import config from "../../config.sample.json";
+// eslint-disable-next-line import/no-unresolved
+import config from "../../config.json";
 
 export const FirebaseService = initializeApp(config);
