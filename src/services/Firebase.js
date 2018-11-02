a0af773695b3eb9a88d72608e6b241c22f46e09f
Yevhenii Ivanisenko <yevhenii.ivanisenko@gmail.com>
2018-11-02 14:26:12 +0200

    Remove firebase config

--- src/services/Firebase.js
+++ src/services/Firebase.js
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import "firebase/database";
 import "firebase/storage";
 
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
 const config = {
-  apiKey: "AIzaSyAAY1HTcEhaBuuPA7qGcA1jy0oeiNcvpEA",
-  authDomain: "spotty-vanilla.firebaseapp.com",
-  databaseURL: "https://spotty-vanilla.firebaseio.com",
-  projectId: "spotty-vanilla",
-  storageBucket: "spotty-vanilla.appspot.com",
-  messagingSenderId: "292173529673"
+  apiKey: "<API_KEY>",
+  authDomain: "<PROJECT_ID>.firebaseapp.com",
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  projectId: "<PROJECT_ID>",
+  storageBucket: "<BUCKET>.appspot.com",
+  messagingSenderId: "<SENDER_ID>"
 };
 
 export const Firebase = initializeApp(config);
