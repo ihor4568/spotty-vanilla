0587c2f5e1b6cae17396da7eb182e3b62f4f3bb6
Ihor Tsarenko <tsarenko.igor1@gmail.com>
2018-11-05 14:20:13 +0200

    KHWE-32 create basic DnD for table

--- src/components/MySongsTable/MySongsTable.js
+++ src/components/MySongsTable/MySongsTable.js
@@ -8,7 +8,9 @@ const TABLE_DATA = [
     name: "Alibaba",
     duration: "9:15",
     artist: "Jazz",
-    album: "Super"
+    album: "Super",
+    id: 1,
+    position: 20
   },
   {
     cover:
@@ -16,15 +18,19 @@ const TABLE_DATA = [
     name: "Timon and Pumba",
     duration: "9:15",
     artist: "Folk",
-    album: "Nice"
+    album: "Nice",
+    id: 2,
+    position: 10
   },
   {
     cover:
       "https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930",
-    name: "Alibaba",
+    name: "baba",
     duration: "9:15",
     artist: "Sympho",
-    album: "The very best"
+    album: "The very best",
+    id: 3,
+    position: 30
   }
 ];
 
@@ -33,6 +39,9 @@ export class MySongsTableComponent {
     this.mountPoint = mountPoint;
     this.props = props;
     this.tableData = TABLE_DATA;
+
+    this.cur = null;
+    this.selected = null;
   }
 
   querySelectors() {
@@ -42,6 +51,8 @@ export class MySongsTableComponent {
     this.dotsMenuPoints = this.mountPoint.querySelectorAll(
       ".my-songs-table__td_more"
     );
+    this.rows = this.mountPoint.querySelectorAll(".my-songs-table__row");
+    this.tableBody = this.mountPoint.querySelector(".my-songs-table__body");
   }
 
   mountChildren() {
@@ -60,6 +71,48 @@ export class MySongsTableComponent {
     window.open("/song/awdklawj");
   }
 
+  // //
+  addEventListeners() {
+    Array.from(this.rows).forEach(row => {
+      row.addEventListener("dragstart", this.dragstart.bind(this));
+    });
+    this.tableBody.addEventListener("dragover", this.dragover.bind(this));
+    this.tableBody.addEventListener("drop", this.drop.bind(this));
+  }
+
+  dragstart(e) {
+    this.cur = parseInt(e.target.id, 10);
+  }
+
+  dragover(e) {
+    e.preventDefault();
+    const row = e.target.closest(".my-songs-table__row");
+    if (row.id) {
+      this.selected = parseInt(row.id, 10);
+    }
+  }
+
+  drop(e) {
+    e.preventDefault();
+
+    const curSongId = this.tableData.findIndex(el => el.id === this.cur);
+    const selectedSongId = this.tableData.findIndex(
+      el => el.id === this.selected
+    );
+
+    if (curSongId !== -1 && selectedSongId !== -1) {
+      const curPos = this.tableData[curSongId].position;
+      this.tableData[curSongId].position = this.tableData[
+        selectedSongId
+      ].position;
+      this.tableData[selectedSongId].position = curPos;
+
+      this.mount();
+    }
+  }
+
+  // //
+
   initMaterial() {
     Array.from(this.iconButtonRipple).forEach(item => {
       this.iconButtonRipple = new MDCRipple(item);
@@ -72,9 +125,13 @@ export class MySongsTableComponent {
     this.querySelectors();
     this.initMaterial();
     this.mountChildren();
+    this.addEventListeners();
   }
 
   render() {
-    return mySongsTableTemplate({ tableData: this.tableData });
+    // return mySongsTableTemplate({ tableData: this.tableData });
+    return mySongsTableTemplate({
+      tableData: this.tableData.sort((a, b) => a.position - b.position)
+    });
   }
 }
