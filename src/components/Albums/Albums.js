67ab479b437a3441eba40af20e30f18fd0c1f21a
Oleg Shynkarenko <oleg.shynkarenko1985@gmail.com>
2018-11-06 00:30:57 +0200

    KHWE-20: Add authors name instead of ids

--- src/components/Albums/Albums.js
+++ src/components/Albums/Albums.js
@@ -26,11 +26,23 @@ export class AlbumsComponent {
 
   fetchAlbumsCollectionData() {
     this.state.isFetching = true;
-    MusicService.getAlbums().then(albums => {
-      this.state.albums = albums;
-      this.state.isFetching = false;
-      this.mount(false);
-    });
+    Promise.all([MusicService.getAlbums(), MusicService.getAuthors()]).then(
+      ([albums, authors]) => {
+        this.state.albums = albums.map(album =>
+          Object.assign(album, {
+            authors: album.authors
+              .map(author => this.getArtistNameById(authors, author))
+              .join(", ")
+          })
+        );
+        this.state.isFetching = false;
+        this.mount(false);
+      }
+    );
+  }
+
+  getArtistNameById(authors, id) {
+    return authors.filter(author => author.id === id)[0].name;
   }
 
   mount(shouldFetchData = true) {
