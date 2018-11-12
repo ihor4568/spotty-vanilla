d346ea07f214776311edf00e9c8f42eae90ccaeb
Ivan Yevsiutin <gamesminer123@gmail.com>
2018-11-13 00:10:35 +0200

    Fix

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -12,8 +12,8 @@ import { AlbumsComponent } from "../Albums/Albums";
 import { AboutComponent } from "../About/About";
 import { ArtistsComponent } from "../Artists/Artists";
 import { NotFoundComponent } from "../NotFound/NotFound";
-import mainTemplate from "./Main.html";
 import { LicenseDialogComponent } from "../LicenseDialog/LicenseDialog";
+import mainTemplate from "./Main.html";
 
 export class MainComponent {
   constructor(mountPoint, props = {}) {
